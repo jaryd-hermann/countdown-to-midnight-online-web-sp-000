a74164a8229a6b133d332327abfd92e9424da490
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-08 22:23:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,14 @@ def countdown(integer)
   puts "HAPPY NEW YEAR!"
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(integer)
+  number = integer
+  while number > 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1.SECOND)
+  end
+  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
+end
